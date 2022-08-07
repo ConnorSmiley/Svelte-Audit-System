e9083247db5c2c6d728868057c6b8fa7aec3ef3b
Connor Smiley <60119998+ConnorSmiley@users.noreply.github.com>
2022-08-08 00:30:44 +0900

    first commit

---
+++ src/components/RecipientsDisplay.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+    import { onMount, tick } from 'svelte'
+
+    //export to AuditTable
+    export let recipients
+
+    //Did not add TS because implied
+    let sentToRecipient = []
+    let badgeNum = 0
+    let element: HTMLDivElement
+    //Used Set in case of duplicates and an ID will be needed in the future
+    const setRecipients = new Set(recipients)
+
+    //Returns a promise
+    const handleResize = async () => {
+        let numOfHiddenMess = 0
+        sentToRecipient = [...setRecipients].map(recipient => ` ${recipient}`)
+
+        //Looping backwards makings trimming easier
+        for (let i = sentToRecipient.length - 1; i > 0; i--) {
+            await tick()
+
+            //Checking width of element
+            const overflow = element.offsetWidth < element.scrollWidth
+            // Change overflow Emails to " ..."
+            if (overflow) sentToRecipient[i] = ' ...'
+            // `?` is used if index is undefined or null
+            if (sentToRecipient[i + 1]?.includes(' ...')) sentToRecipient.pop()
+        }
+
+        numOfHiddenMess += recipients.length - sentToRecipient.length
+        //From the "for loop", don't want to mutate the array
+        if (sentToRecipient[sentToRecipient.length - 1].includes(' ...')) numOfHiddenMess++
+        badgeNum = numOfHiddenMess
+
+    }
+
+    onMount(() => {
+        handleResize()
+    })
+    window.addEventListener('resize', handleResize)
+</script>
+
+<style lang="scss">
+  section {
+    display:flex;
+    align-items: center;
+
+
+  }
+  div.recipient {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 16px;
+    color: #333333;
+    padding: 5px 10px;
+
+  }
+  div.badgeNum {
+    font-size: 16px;
+    color: #f0f0f0;
+    background-color: $color-primary;
+    border-radius: 3px;
+    padding: 2px 5px;
+    margin-left: auto;
+  }
+</style>
+
+<section>
+    <div class="recipient" bind:this={element}>{sentToRecipient}</div>
+    {#if badgeNum > 0}
+        <div class="badgeNum">+ {badgeNum}</div>
+    {/if}
+</section>
